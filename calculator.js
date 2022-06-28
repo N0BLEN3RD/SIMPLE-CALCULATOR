7e0363fae43b1218167fea06fa78d8124faccc91
N0BLEN3RD <105947102+N0BLEN3RD@users.noreply.github.com>
2022-06-28 23:50:06 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,17 @@
+alert('You successfully linked your JavaScript file!');
+// This function clear all the values
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+ 
+// This function display values
+function display(value) {
+    document.getElementById("result").value += value;
+}
+ 
+// This function evaluates the expression and returns result
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
